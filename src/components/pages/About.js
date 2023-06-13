6a31450d39b9deb00252e1e1bb2001843d029e87
alec <amarierose19@example.com>
2023-06-13 19:49:48 -0400

    finished about me section

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ export default function About() {
     <div>
       <h1>About Me</h1>
       <p>
-        
+        My name is Alec Marierose and I am a graduate of the Georgia Tech Bootcamp coding class. I am a recently graduate from liberty University with a Masters and Bachelors degree in Sport Management. I enjoy learning new languages in tech such as CSS, HTML JavaScript, and much more. 
       </p>
     </div>
   );
